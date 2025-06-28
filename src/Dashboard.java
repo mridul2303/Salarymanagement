04525905739d3e8aff3c615c69c2ce3ea6e64122
mridul2303 <mridul2303@gmail.com>
2025-06-28 19:05:04 +0530

    Dashboard almost complete

--- src/Dashboard.java
+++ src/Dashboard.java
@@ -1,12 +1,15 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-class Dashboard extends Frame implements ActionListener {
-    Button addincome = new Button("Add Income");
-    Button viewincome = new Button("view Income");
-    Button addexpense = new Button("Add Expense");
-    Button viewexpense = new Button("view Expense");
+class Dashboard extends Frame implements ActionListener, MouseListener {
+
+    Label addincome = new Label("Add Income");
+    Label viewincome = new Label("view Income");
+    Label addexpense = new Label("Add Expense");
+    Label viewexpense = new Label("view Expense");
 
     PopupMenu popupMenu1 = new PopupMenu();//add income
     MenuItem fixedincome1 = new MenuItem("add Fix Income");
@@ -18,7 +21,7 @@ class Dashboard extends Frame implements ActionListener {
 
     PopupMenu popupMenu3 = new PopupMenu();//add exp
     MenuItem tempExp3 = new MenuItem("add Temprory Expense");
-    MenuItem fixemp3 = new MenuItem("add Fix Expense");
+    MenuItem fixedexp3 = new MenuItem("add Fix Expense");
 
     PopupMenu popupMenu4 = new PopupMenu();//view exp
     MenuItem tempExp4 = new MenuItem("View Temprory Expense");
@@ -37,16 +40,68 @@ class Dashboard extends Frame implements ActionListener {
         //view income
         add(viewincome);add(popupMenu2);popupMenu2.add(fixedincome2);popupMenu2.add(tempincome2);
         //add exmp
-        add(addexpense);add(popupMenu3);popupMenu3.add(fixemp3);popupMenu3.add(tempExp3);
+        add(addexpense);add(popupMenu3);popupMenu3.add(fixedexp3);popupMenu3.add(tempExp3);
         //view exp
         add(viewexpense);add(popupMenu4);popupMenu4.add(fixedexp4);popupMenu4.add(tempExp4);
 
-        addincome.addActionListener(this);viewincome.addActionListener(this);
-        addexpense.addActionListener(this);viewexpense.addActionListener(this);
+        addincome.addMouseListener(this);
+        viewincome.addMouseListener(this);
+        addexpense.addMouseListener(this);
+        viewexpense.addMouseListener(this);
 
+fixedincome1.addActionListener(this);tempincome1.addActionListener(this);
+fixedincome2.addActionListener(this);tempincome2.addActionListener(this);
+fixedexp3.addActionListener(this);tempExp3.addActionListener(this);
+fixedexp4.addActionListener(this);tempExp4.addActionListener(this);
     }
+
+    public void mouseClicked (MouseEvent e){
+        Object o = e.getSource();
+        if (o==addincome){
+            popupMenu1.show(addincome,0,addincome.getHeight());
+        }
+        if (o==viewincome){
+            popupMenu2.show(viewincome,0,viewincome.getHeight());
+        }
+        if (o==addexpense){
+            popupMenu3.show(addexpense,0,addexpense.getHeight());
+        }
+        if (o==viewexpense){
+            popupMenu4.show(viewexpense,0,viewexpense.getHeight());
+        }
+    }
+
+    public void mousePressed(MouseEvent e) {}
+    public void mouseReleased(MouseEvent e) {}
+    public void mouseEntered(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) {}
+
+    //task if clicked
     public void actionPerformed (ActionEvent e){
-
-
+        Object o = e.getSource();
+        if (o==fixedincome1){
+            System.out.println("hello");
+        }
+        if (o==tempincome1){
+            System.out.println("hello");
+        }
+        if (o==fixedincome2){
+            System.out.println("hello");
+        }
+        if (o==tempincome2){
+            System.out.println("hello");
+        }
+        if (o==tempExp3){
+            System.out.println("hello");
+        }
+        if (o==fixedexp3){
+            System.out.println("hello");
+        }
+        if (o==tempExp4){
+            System.out.println("hello");
+        }
+        if (o==fixedexp4){
+            System.out.println("hello");
+        }
     }
 }
